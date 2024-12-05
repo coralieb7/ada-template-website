d93a210c3b8b5ce842de07b184331d816863752d
Coralie Banuls <coraliebanuls@tsf-428-wpa-5-017.epfl.ch>
2024-12-05 15:21:30 +0100

    animated texts

--- assets/js/typing.js
+++ assets/js/typing.js
@@ -1,51 +1,45 @@
-const questions = [
-    'Let\'s play a game!',
-    'Prompt: The most similar word to "England" among the links in the "Africa" Wikipedia page is:',
-    'Prompt: The most similar word to "England" among the links in the "England" Wikipedia page is:',
-    'You reached the end of the game!'
-];
-const answers = [
-    'Instruction: You have to reach "England" starting from "Africa".',
-    'LLM: Europe',
-    'LLM: England',
-    'Well done!'
+const texts = [
+    'Do you love movies?',
+    'What proportion of female characters is portrayed as smart in movies?',
+    'What is the impact of the #MeToo movement on the film industry?',
+    'How do you think the film industry can be more inclusive?',
+    'In your opinion, how are women portrayed in movies?',
+    'What are the prime years for an actress?',
+    'How many female directors do you know?',
+    'Don\'t know half of the answers? You are in the right place.'
 ];
 
-
-let currentPair = 0;
-let animationFrameRequest; 
+let currentTextIndex = 0;
+let animationFrameRequest;
 
 function typeWriter(text, elementId, callback) {
     let i = 0;
     const element = document.getElementById(elementId);
     element.textContent = '';
-    
+
     function typing() {
         if (i < text.length) {
             element.textContent += text.charAt(i);
             i++;
-            animationFrameRequest = setTimeout(typing, 100);
+            animationFrameRequest = setTimeout(typing, 100); // Speed of typing
         } else if (callback) {
-            animationFrameRequest = setTimeout(callback, 1000);
+            animationFrameRequest = setTimeout(callback, 1000); // Delay before the next text
         }
     }
-    
+
     typing();
 }
 
-function showQuestionAnswerPair() {    
-    typeWriter(questions[currentPair], 'anim', () => {
-        setTimeout(() => {
-            typeWriter(answers[currentPair], 'anim', () => {
-                currentPair = (currentPair + 1) % questions.length;
-                showQuestionAnswerPair();
-            });
-        }, 1000);
+function showTextsLoop() {
+    typeWriter(texts[currentTextIndex], 'anim', () => {
+        currentTextIndex = (currentTextIndex + 1) % texts.length;
+        showTextsLoop(); // Call the function again for the next text
     });
 }
 
 window.onbeforeunload = function () {
     window.scrollTo(0, 0);
-}
+};
 
-showQuestionAnswerPair();
+// Start the typewriter loop
+showTextsLoop();
